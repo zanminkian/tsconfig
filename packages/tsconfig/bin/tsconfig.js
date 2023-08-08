6d359c6c63fb3fcfb44c8f9150b7836f4f1f14ee
zanminkian <hellozmj@qq.com>
2023-08-08 15:32:28 +0000

    style: format

--- packages/tsconfig/bin/tsconfig.js
+++ packages/tsconfig/bin/tsconfig.js
@@ -1,5 +1,5 @@
-import { resolve } from 'node:path'
 import fs from 'node:fs'
+import { resolve } from 'node:path'
 import process from 'node:process'
 import { Command } from 'commander'
 
@@ -15,16 +15,15 @@ const generatingTsconfigContent = `{
 
 const program = new Command()
 program.name('tsconfig')
-program.command('init')
+program
+  .command('init')
   .description('init a tsconfig file')
   .option('-t, --to <path>', 'directory that generating to', '.')
   .option('-n, --name <filename>', 'tsconfig file name', 'tsconfig.json')
   .action(({ to, name }) => {
     const fullName = resolve(process.cwd(), to, name)
-    if (fs.existsSync(fullName))
-      throw new Error(`${fullName} is already existing!`)
-    else
-      fs.writeFileSync(fullName, generatingTsconfigContent)
+    if (fs.existsSync(fullName)) throw new Error(`${fullName} is already existing!`)
+    else fs.writeFileSync(fullName, generatingTsconfigContent)
   })
 
 program.parse()
